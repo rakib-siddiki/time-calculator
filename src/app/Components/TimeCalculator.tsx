5d48f305b275c6cd34686382e97e5e9c37fc0677
rakib-siddiki <rmrakib901@gmail.com>
2025-06-04 21:28:35 +0600

    feat: add shadCn

---
+++ src/app/Components/TimeCalculator.tsx
@@ -0,0 +1,13 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
+const TimeCalculator = () => {
+  return (
+    <Card className="max-w-2xl w-full mx-auto">
+      <CardHeader>
+        <CardTitle>Time Calculator</CardTitle>
+      </CardHeader>
+    </Card>
+  );
+};
+
+export default TimeCalculator;
